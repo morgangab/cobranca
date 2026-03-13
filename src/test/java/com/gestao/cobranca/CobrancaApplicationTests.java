0f730b90164fcd052a54ae3e8582a9ff9fd08ca9
Gabriel Morgan <morggab42@gmail.com>
2026-03-12 22:07:08 -0300

    Início do projeto de gestão de cobrança

---
+++ src/test/java/com/gestao/cobranca/CobrancaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestao.cobranca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CobrancaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
